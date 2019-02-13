68fd0d6ed0550e6e0e2f42815d961c5dfedcfb8e
Danny Arica <danny.arica@outlook.com>
2019-02-12 23:26:05 -0500

    Sending events to Seq
    Signed-off-by: Danny Arica <danny.arica@outlook.com>

---
+++ netframework/SerilogDemo/Account.cs
@@ -0,0 +1,10 @@
+﻿namespace SeriLogDemo
+{
+    public class Account
+    {
+        public int AccountId { get; set; }
+        public string Identifier { get; set; }
+        public string Name { get; set; }
+        public string Phone { get; set; }
+    }
+}
---
+++ netframework/SerilogDemo/Program.cs
@@ -0,0 +1,39 @@
+﻿using Serilog;
+using SeriLogDemo;
+using System;
+
+namespace SerilogDemo
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            using (var log = new LoggerConfiguration()
+                            .Enrich.WithProperty("Environment", "Development")
+                            .Enrich.WithProperty("Version", "1.0.0")
+                            .Enrich.WithProperty("Application", "CajaTrujillo-App")
+                            .MinimumLevel.Debug()
+                            .WriteTo.Seq("http://localhost:5341")
+                            .WriteTo.File("log-.txt",
+                                            outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {Message:lj}{NewLine}{Exception}",
+                                            rollingInterval: RollingInterval.Day)
+                            .CreateLogger())
+            {
+                //log.Information("Ah, there you are!");
+
+                var account = new Account
+                {
+                    AccountId = 2706,
+                    Identifier = Guid.NewGuid().ToString(),
+                    Name = "Luis Fonsi",
+                    Phone = "+51944589632"
+                };
+
+                //log.Debug("This is the Account object => {@account}", account);
+                
+                log.Error("An error was found in Account {@account}", account, new Exception("no content file"));
+            }
+            Console.ReadKey();
+        }
+    }
+}
